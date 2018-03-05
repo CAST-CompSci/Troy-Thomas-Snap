195113209d44f21a2f0aef7acaa1f775ed050a8d
Troy Edkins <troyalexanderedkins@gmail.com>
2018-03-05 16:12:56 +0000

    Added Card System and Main
    Added the Card structure, player stacks and queues and Main sub

--- Snap Game/Program.cs
+++ Snap Game/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Snap_Game
 {
+    struct card
+    {
+        string number, suitSymbol;
+        Boolean isUsed;
+    }
+    
     class Program
     {
+        static Queue<card> playerOneQueue = new Queue<card>(52);
+        static Queue<card> playerTwoQueue = new Queue<card>(52);
+        static Stack<card> playerOneStack = new Stack<card>(52);
+        static Stack<card> playerTwoStack = new Stack<card>(52);
+
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Menu();
+                AddCards();
+                Game();
+            }
         }
     }
 }
