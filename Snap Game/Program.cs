c848eee468c0c773cddc77b4d0b2155e28757673
Troy Edkins <troyalexanderedkins@gmail.com>
2018-03-08 14:44:45 +0000

    Added Game Sub

--- Snap Game/Program.cs
+++ Snap Game/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Snap_Game
 {
-    struct card
+    struct Card
     {
         public string number, suitSymbol;
         public Boolean isUsed;
@@ -14,10 +14,10 @@ namespace Snap_Game
     
     class Program
     {
-        static Queue<card> playerOneQueue = new Queue<card>(52);
-        static Queue<card> playerTwoQueue = new Queue<card>(52);
-        static Stack<card> playerOneStack = new Stack<card>(52);
-        static Stack<card> playerTwoStack = new Stack<card>(52);
+        static Queue<Card> playerOneQueue = new Queue<Card>(52);
+        static Queue<Card> playerTwoQueue = new Queue<Card>(52);
+        static Stack<Card> playerOneStack = new Stack<Card>(52);
+        static Stack<Card> playerTwoStack = new Stack<Card>(52);
 
 
         static void Main(string[] args)
@@ -47,7 +47,7 @@ namespace Snap_Game
 
         static void AddCards()
         {
-            card[] cardArray = new card[52];
+            Card[] cardArray = new Card[52];
             int loopCount = 0;
             //Create cards in array
             for (int a = 0; a <= 4; a++)
@@ -123,9 +123,55 @@ namespace Snap_Game
                 }
                 else
                 {
-                    playerOneQueue.Enqueue(cardArray[randomNumber]);
+                    playerTwoQueue.Enqueue(cardArray[randomNumber]);
                     cardArray[randomNumber].isUsed = true;
                 }
+            }
+        }
+
+        static void Game()
+        {
+            while (true)
+            {
+                if (playerOneQueue.Count == 52)
+                {
+                    Win("Player 1");
+                }
+                else if (playerTwoQueue.Count == 52)
+                {
+                    Win("Player 2");
+                }
+                playerOneStack.Push(playerOneQueue.Dequeue());
+                playerTwoStack.Push(playerTwoQueue.Dequeue());
+                Console.CursorLeft = 0;
+                displayCard(playerOneStack.Peek());
+                Console.CursorLeft = 20;
+                displayCard(playerTwoStack.Peek());
+                ConsoleKeyInfo key = Console.ReadKey();
+                System.Threading.Thread.Sleep(1000);
+                if (key.Key == ConsoleKey.Z)
+                {
+                    if (playerOneStack.Peek().number ==playerTwoStack.Peek().number)
+                    {
+                        playerOneScore();
+                    }
+                    else
+                    {
+                        playerTwoScore();
+                    }
+                }
+                else if (key.Key == ConsoleKey.M)
+                {
+                    if (playerOneStack.Peek().number == playerTwoStack.Peek().number)
+                    {
+                        playerTwoScore();
+                    }
+                    else
+                    {
+                        playerOneScore();
+                    }
+                }
+
             }
         }
 
